e3ec4fb010445a43cf9fb0aa5e8171486b64fcc8
sanchita <mac@Macs-MacBook-Pro.local>
2021-01-25 13:23:28 +0530

    first commit

---
+++ Authentication/Batch.swift
@@ -0,0 +1,9 @@
+//
+//  Batch.swift
+//  Authentication
+//
+//  Created by Mac on 14/01/21.
+//  Copyright © 2021 Sanchita. All rights reserved.
+//
+
+import Foundation
---
+++ Authentication/Switcher.swift
@@ -0,0 +1,9 @@
+//
+//  Switcher.swift
+//  Authentication
+//
+//  Created by Mac on 02/01/21.
+//  Copyright © 2021 Sanchita. All rights reserved.
+//
+
+import Foundation
